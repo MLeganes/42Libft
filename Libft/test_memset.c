94e7584ccbf45efaf4d8dd2e1cf47f15a56b96bd
Angel Morcillo leganes <amorcill@1-A-14.42heilbronn.de>
2021-06-22 19:29:18 +0200

    norminette

--- Libft/test_memset.c
+++ Libft/test_memset.c
@@ -6,24 +6,24 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 11:36:03 by amorcill          #+#    #+#             */
-/*   Updated: 2021/06/22 12:53:04 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/06/22 19:23:22 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-void *ft_memset(void *s, int c, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 
-void test_memset(){
+void	test_memset(void)
+{
+	char	s[50];
 
-    char s[50] = "Hello schol42Heilbronn";
-    char ss[50] = "Hello schol42Heilbronn";
-
-    ft_memset(s, '*', 5);
-    printf("[Test ft_memset] ft_memset: %s\n", s);
-    
-    memset(ss, '.', 5);
-    printf("[Test ft_memset] memset: %s\n", ss);
-
+	printf("\n[test memset ...]\n");
+	strcpy(s, "Hello schol42Heilbronn");
+	ft_memset(s, '*', 5);
+	printf("[Test ft_memset] ft_memset: %s\n", s);
+	strcpy(s, "Hello schol42Heilbronn");
+	memset(s, '.', 5);
+	printf("[Test memset]       memset: %s\n", s);
 }
