fa384da6fcfb7f3dd15d92ee95d76e33d91beb76
Angel Morcillo leganes <amorcill@1-C-7.42heilbronn.de>
2021-06-23 16:05:05 +0200

    libft.h all in normi

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,17 +6,17 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 12:18:31 by amorcill          #+#    #+#             */
-/*   Updated: 2021/06/23 12:54:54 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/06/23 16:01:48 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stdio.h>
-#include <string.h>
+# include <stdio.h>
+# include <string.h>
 
-void    *ft_memset(void *s, int c, size_t n);
-size_t  ft_strnlen(const char *ch);
+void	*ft_memset(void *s, int c, size_t n);
+size_t	ft_strnlen(const char *ch);
 
 #endif
