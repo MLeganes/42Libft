d8b06f12e55da9da03920067c80b86ca8001a145
Angel Morcillo leganes <amorcill@1-C-8.42heilbronn.de>
2021-06-25 13:30:09 +0200

    strlen ok normi ok

--- Libft/ft_strlen.c
+++ Libft/ft_strlen.c
@@ -6,18 +6,18 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 14:12:32 by amorcill          #+#    #+#             */
-/*   Updated: 2021/06/23 12:56:01 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/06/25 13:28:24 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strnlen(const char *ch)
+size_t	ft_strlen(const char *ch)
 {
 	int	count;
 
 	count = 0;
-	while (ch[count] != '\0')
+	while (*(ch + count))
 	{
 		count++;
 	}
