35f6822890957b1e6e783bad214e72fe245d6bfd
Angel Morcillo leganes <amorcill@1-C-8.42heilbronn.de>
2021-06-26 16:40:52 +0200

    add isalnun func

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 12:18:31 by amorcill          #+#    #+#             */
-/*   Updated: 2021/06/26 15:08:19 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/06/26 16:24:50 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,13 @@ size_t	ft_strlen(const char *ch);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-int     ft_isalpha(int c);
-int     ft_isdigit(int c);
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
 
 #endif
