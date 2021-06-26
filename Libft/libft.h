15df4c25d0a70961c3355e0b894f2ced181c6dda
Angel Morcillo leganes <amorcill@1-C-8.42heilbronn.de>
2021-06-26 15:00:21 +0200

    isascii funcn, libt and working normi ok

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,23 +6,24 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 12:18:31 by amorcill          #+#    #+#             */
-/*   Updated: 2021/06/26 14:33:25 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/06/26 14:55:34 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-# define LIBFT_H
+#define LIBFT_H
 
-# include <stdio.h>
-# include <string.h>
+#include <stdio.h>
+#include <string.h>
 
 void	*ft_memset(void *s, int c, size_t n);
 size_t	ft_strlen(const char *ch);
-void    *ft_memcpy(void *dst, const void *src, size_t n);
-size_t  ft_strlcat(char *dst, const char *src, size_t size);
-size_t  ft_strlcpy(char *dst, const char *src, size_t size);
-int     ft_tolower(int c);
-int     ft_toupper(int c);
-int     ft_isprint(int c);
-     
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
+
 #endif
