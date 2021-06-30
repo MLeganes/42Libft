bfaefe21f9ea97c9daecb549630d510681a38e84
Angel Morcillo leganes <amorcill@1-C-8.42heilbronn.de>
2021-06-30 12:18:24 +0200

    strNcmp fix bug and normi

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,27 +6,26 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 15:51:21 by amorcill          #+#    #+#             */
-/*   Updated: 2021/06/29 16:46:47 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/06/30 12:17:19 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int     ft_strncmp(const char *s1, const char *s2, size_t n)
-{	
-	unsigned char *s1p;
-	unsigned char *s2p;
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned char	*s1p;
+	unsigned char	*s2p;
 
 	s1p = (unsigned char *)s1;
 	s2p = (unsigned char *)s2;
-
-	while ( *s1p != '\0' && *s2p != '\0' && *s1p == *s2p && n != 0)
+	while (*s1p != '\0' && *s2p != '\0' && *s1p == *s2p && n != 0)
 	{
 		s1p++;
 		s2p++;
 		n--;
 	}
-	if (n == 0 && *s1 == *s2)
+	if (n == 0)
 		return (0);
-	return ( (*s1p) - (*s2p));	
+	return ((*s1p) - (*s2p));
 }
