89eb839b2f9fb004aebbfafa905d58f0472ffe2a
Angel Morcillo leganes <amorcill@1-B-8.42heilbronn.de>
2021-07-02 18:43:12 +0200

    str l cat crazy

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,19 +6,33 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 16:25:43 by x250              #+#    #+#             */
-/*   Updated: 2021/06/25 08:56:40 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/07/02 18:41:52 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t buffer)
 {
-	size_t len;
+	size_t	counter;
+	size_t	s;
+	size_t	d;	
 
-	len = 0;
-	while( dst[len] && src[len] && len < (size - 1)){
-		len++;
+	d = ft_strlen(dst);
+	s = ft_strlen(src);
+	if (buffer != 0)
+	{
+		counter = 0;
+		while (d + counter < (buffer - 1) && src[counter] != '\0')
+		{
+			dst[d + counter] = src[counter];
+			counter++;
+		}
+		if (d + counter < buffer)
+			dst[d + counter] = '\0';
 	}
-	return(len);
+	if (buffer > d)
+		return (d + s);
+	return (buffer + s);
 }
