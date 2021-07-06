48ece9e62a108510f5800fb3f3c8db78b000024f
MLeganes <a.eme.ele@gmail.com>
2021-07-06 17:04:20 +0200

    mem cpy

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
+/*   By: x250 <x250@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 11:56:41 by amorcill          #+#    #+#             */
-/*   Updated: 2021/06/28 13:35:46 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/07/06 16:56:39 by x250             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t		len;
+	size_t			len;
+	unsigned char	*pd;
+	unsigned char	*ps;
 
 	if (!dst && !src)
 		return (NULL);
+	pd = (unsigned char *)dst;
+	ps = (unsigned char *)src;
 	len = 0;
 	while (len < n)
-	{		
-		*(char *)(dst + len) = *(char *)(src + len);
+	{
+		pd[len] = ps[len];
 		len++;
 	}
 	return (dst);
