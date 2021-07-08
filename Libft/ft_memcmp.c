a01c85308bf908a4019cd51863c46a9a662c1b9b
Angel Morcillo leganes <amorcill@3-F-5.42heilbronn.de>
2021-07-08 18:31:59 +0200

    somethign chaged

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: x250 <x250@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 23:52:18 by x250              #+#    #+#             */
-/*   Updated: 2021/07/07 00:15:01 by x250             ###   ########.fr       */
+/*   Updated: 2021/07/08 18:21:05 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*ps1;
-	unsigned char	*ps2;
+	unsigned const char	*ps1;
+	unsigned const char	*ps2;
 
-	ps1 = (unsigned char *)s1;
-	ps2 = (unsigned char *)s2;
-	while (n > 0 && *ps2 != '\0' && *ps1 != '\0' && *ps1 == *ps2)
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+	ps1 = (unsigned const char *)s1;
+	ps2 = (unsigned const char *)s2;
+	while (n > 0 && *ps1 == *ps2)
 	{
 		ps1++;
 		ps2++;
