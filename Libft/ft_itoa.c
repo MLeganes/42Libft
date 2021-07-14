969315d846bb2502bf6906af3845bfae61a4ac33
Angel Morcillo leganes <amorcill@1-B-8.42heilbronn.de>
2021-07-14 19:50:07 +0200

    Works ITOA

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -3,46 +3,77 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: x250 <x250@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 16:59:02 by amorcill          #+#    #+#             */
-/*   Updated: 2021/07/14 01:42:07 by x250             ###   ########.fr       */
+/*   Updated: 2021/07/14 19:48:12 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// char	*ft_itoa(int n)
-// {
-// 	char	*ret;
-// 	int		res;
-// 	char	resto;
-// 	long	nll;
+static int	ft_intlen(int n)
+{
+	int		count;
+	int		sign;
 
-// 	if (n == -2147483648)
-// 		return (ft_strdup("-2147483648"));
-// 	nll = (long)n;
-// 	if (n == 0 || n > 2147483647 || n < -2147483647)
-// 		return (ft_strdup("0"));
-// 	ret = (char *)malloc(12);
-// 	ft_bzero(ret, 12);
-// 	res = n;
-// 	if (n < 0)
-// 		res = n * (-1);
-// 	ret = ret + 11;
-// 	while (res != 0)
-// 	{
-// 		resto = (res % 10) + '0';
-// 		*ret = resto;
-// 		ret--;
-// 		res = res / 10;
-// 	}
-// 	ret++;
-// 	if (n < 0)
-// 	{
-// 		ret--;
-// 		*ret = '-';
-// 	}
-// 	return (ret);
-	
-// }
+	sign = 1;
+	if (n < 0)
+	{
+		sign = 0;
+		n = n * -1;
+	}
+	count = 0;
+	while (n != 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	if (sign == 0)
+		count++;
+	return (count);
+}
+
+static char	*ft_inttostr(int sign, int n, char *ret)
+{
+	while (n != 0)
+	{
+		*ret = (n % 10) + '0';
+		ret--;
+		n = n / 10;
+	}
+	ret++;
+	if (sign == 0)
+	{
+		ret--;
+		*ret = '-';
+	}
+	return (ret);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*ret;
+	int		nlen;
+
+	if (n == 0 || n == -0)
+		return (ft_strdup("0"));
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	if (n == 2147483647)
+		return (ft_strdup("2147483647"));
+	nlen = ft_intlen(n);
+	ret = (char *)malloc((char)nlen + 1);
+	if (ret == 0)
+		return (0);
+	ft_bzero(ret, nlen + 1);
+	ret = ret + nlen - 1;
+	if (n < 0)
+	{
+		n = n * (-1);
+		ret = ft_inttostr(0, n, ret);
+	}
+	else
+		ret = ft_inttostr(1, n, ret);
+	return (ret);
+}
