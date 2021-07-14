585cbf4383606627424f9a8eed68488c2c24e469
MLeganes <a.eme.ele@gmail.com>
2021-07-14 10:51:56 +0200

    no working, to have it in school

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -3,46 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
+/*   By: x250 <x250@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 16:59:02 by amorcill          #+#    #+#             */
-/*   Updated: 2021/07/13 16:59:05 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/07/14 01:42:07 by x250             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
-{
-	char	*ret;
-	int		res;
-	char	resto;
-	long	nll;
+// char	*ft_itoa(int n)
+// {
+// 	char	*ret;
+// 	int		res;
+// 	char	resto;
+// 	long	nll;
 
-	if (n == -2147483648)
-		return (ft_strdup("-2147483648"));
-	nll = (long)n;
-	if (n == 0 || n > 2147483647 || n < -2147483647)
-		return (ft_strdup("0"));
-	ret = (char *)malloc(12);
-	ft_bzero(ret, 12);
-	res = n;
-	if (n < 0)
-		res = n * (-1);
-	ret = ret + 11;
-	while (res != 0)
-	{
-		resto = (res % 10) + '0';
-		*ret = resto;
-		ret--;
-		res = res / 10;
-	}
-	ret++;
-	if (n < 0)
-	{
-		ret--;
-		*ret = '-';
-	}
-	return (ret);
+// 	if (n == -2147483648)
+// 		return (ft_strdup("-2147483648"));
+// 	nll = (long)n;
+// 	if (n == 0 || n > 2147483647 || n < -2147483647)
+// 		return (ft_strdup("0"));
+// 	ret = (char *)malloc(12);
+// 	ft_bzero(ret, 12);
+// 	res = n;
+// 	if (n < 0)
+// 		res = n * (-1);
+// 	ret = ret + 11;
+// 	while (res != 0)
+// 	{
+// 		resto = (res % 10) + '0';
+// 		*ret = resto;
+// 		ret--;
+// 		res = res / 10;
+// 	}
+// 	ret++;
+// 	if (n < 0)
+// 	{
+// 		ret--;
+// 		*ret = '-';
+// 	}
+// 	return (ret);
 	
-}
+// }
--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -3,20 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
+/*   By: x250 <x250@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 11:23:56 by x250              #+#    #+#             */
-/*   Updated: 2021/07/13 18:04:06 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/07/14 10:50:50 by x250             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int ft_countwords(char *s, char c)
-{
-	int	words;
-	int	isword;
 
+static 	int ft_countwords(const char *str, char c)
+{
+	int		words;
+	int		isword;
+	char	*s;
+
+	s = (char *)str;
 	words = 0;
 	isword = 0;
 	while( *s != '\0')
@@ -25,59 +28,174 @@ static int ft_countwords(char *s, char c)
 		{
 			isword = 0;			
 		}
-		else 
+		else if (isword == 0)
 		{
-			isword = 1 
-			//letter from word
-		}
-		if (isword == 1)
-		{
-			
+			isword = 1;
+			words++;
 		}
 		s++;
-		
-		
 	}
+	return (words);
+}
+
+// static char *ft_findword(char *str, char c)
+// {
+//     ft_strchr( s, c)
+// }
+
+static char	*ft_sub(char const *s, unsigned int start, int len)
+{
+	char	*substr;
+	int     counter;
+
+	counter = 0;
+	substr = (char *)malloc(len + 1);
+	
+    if (substr == NULL )
+        return (NULL);
+
+	while (counter < len && s[counter] != '\0')
+	{
+		substr[counter] = s[start + counter];
+		counter++;
+	}
+	substr[counter] = '\0';	
+	return (substr);
+}
+
+static void ft_findworkd()
+{
+
 }
 
 char **ft_split(char const *s, char c)
 {
-	char const	*s1;
-	char 	*result;
+	char    **result;
+    char    *start_word;
+    char    *end_word;
+    char    *word;
 	int 	words;
-	int 	len;
+    int     counter;
+    int     lenword;
+    int     isword;
 
-	
 	words = ft_countwords(s, c);
+    result = (char **)malloc( sizeof(char **) * (words + 1)); // words + 1 ending with null. (char *) = 1???????
+    if (result == 0)
+        return (0);  //malloc protected == 0 or null
+
+    start_word = (char *)s;
+    end_word = (char *)s;
+    counter = 0;
+
+    lenword = 0;
+    isword = 0;
 
 
+    //look for the word and sub-workd
 
 
-	s1 = s;
-	len = ft_strlen(s);
-	result = malloc( len);
-	if (!result)
-		return (NULL);
-	i = 0;
-
-
-
-	
-	while (*s1)
-	{
-		if (*s1 == c)
-			s1++;
-		else
+    
+    while( *s != '\0')
+    {
+        if ( *s == c)
 		{
-			while ( *s1 != c && *s1 != '\0')
-			{
-				result[i] = *s1;
-				s1++;
-				i++;
-			}
-			result[i] = '\n';
+            if (isword == 1)
+            {
+                end_word = (char *)s;
+                ///ft_memcpy or strcpy or somethign to copy the word in the result.
+                lenword = start_word - end_word;
+                if ( lenword < 0)
+                    lenword = lenword * (-1);
+
+                word = ft_sub(start_word, 0, end_word - start_word);
+                result[counter] = word;
+                counter++;
+            }
+			isword = 0;
 		}
-	}
-	
+		else if (isword == 0)
+		{
+			isword = 1;
+			start_word = (char *)s;
+		}
+		s++;
+    }
+    result[counter] = NULL;
 	return (result);
 }
+
+
+/*****
+ * 
+ * To DELETEEEE
+ * 
+ * 
+ */ 
+
+// static 	int ft_countwords(char *str, char c)
+// {
+// 	int		words;
+// 	int		isword;
+// 	char	*s;
+
+// 	s = str;
+// 	words = 0;
+// 	isword = 0;
+// 	while( *s != '\0')
+// 	{
+// 		if ( *s == c)
+// 		{
+// 			isword = 0;			
+// 		}
+// 		else if (isword = 0)
+// 		{
+// 			isword = 1;
+// 			words++;
+// 		}
+// 		s++;
+// 	}
+// 	return (words);
+// }
+
+// char **ft_split(char const *s, char c)
+// {
+// 	char const	*s1;
+// 	char 	*result;
+// 	int 	words;
+// 	int 	len;
+
+	
+// 	words = ft_countwords(s, c);
+
+
+
+
+// 	s1 = s;
+// 	len = ft_strlen(s);
+// 	result = malloc( len);
+// 	if (!result)
+// 		return (NULL);
+// 	i = 0;
+
+
+
+	
+// 	while (*s1)
+// 	{
+// 		if (*s1 == c)
+// 			s1++;
+// 		else
+// 		{
+// 			while ( *s1 != c && *s1 != '\0')
+// 			{
+// 				result[i] = *s1;
+// 				s1++;
+// 				i++;
+// 			}
+// 			result[i] = '\n';
+// 		}
+// 	}
+	
+// 	return (result);
+// }
